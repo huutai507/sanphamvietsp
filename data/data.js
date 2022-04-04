99085ff413456ff3efaabaf8bdde62a82642b847
Whutai <taiphantmt@gmail.com>
2022-04-04 10:57:14 +0700

    update

--- data/data.js
+++ data/data.js
@@ -1,8 +1,10 @@
 export const naturalhuta = [
   {
     name: "1",
-    afflink: "https://amzn.to/3IPgwMD",
-    imglink: "https://m.media-amazon.com/images/I/51mnwut4YAL._SL350_.jpg"
+    afflink:
+      "https://www.lazyshib.shop/collections/pets/products/usb-rechargeable-rgb-led-color-changing-light-up-pet-dog-collar-flashing-glow-glowing-luminous-night-dog-collars?variant=1000008332552424",
+    imglink:
+      "//ae01.alicdn.com/kf/H85072baf6fa34ef787e52111311a5d55m.jpg_350x350.jpg"
   }
 ];
 export const amazongallerytrending = [
