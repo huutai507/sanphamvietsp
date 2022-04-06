c2e5a127b65a243dd6acea83f023559cd4c8710e
Whutai <taiphantmt@gmail.com>
2022-04-06 08:57:09 +0700

    update

--- data/data.js
+++ data/data.js
@@ -1,17 +1,115 @@
 export const naturalhuta = [
   {
-    name: "2",
-    afflink:
-      "https://www.lazyshib.shop/products/perfume-atomizer-portable-liquid-container",
-    imglink:
-      "https://img.btdmp.com/10312/10312716/products/0x540@1648993054bc4846ad69.jpeg"
-  },
-  {
     name: "1",
     afflink:
       "https://www.lazyshib.shop/collections/pets/products/usb-rechargeable-rgb-led-color-changing-light-up-pet-dog-collar-flashing-glow-glowing-luminous-night-dog-collars?variant=1000008332552424",
     imglink:
       "//ae01.alicdn.com/kf/H85072baf6fa34ef787e52111311a5d55m.jpg_350x350.jpg"
+  },
+  {
+    name: "16",
+    afflink:
+      "https://www.lazyshib.shop/collections/pets/products/smart-sensing-interactive-cat-toys-automatic-electronic-snake",
+    imglink:
+      "https://img.btdmp.com/10312/10312716/products/0x540@1648989085cf6a8161ff.gif"
+  },
+  {
+    name: "15",
+    afflink:
+      "https://www.lazyshib.shop/collections/pets/products/self-cleaning-slicker-brush-for-dogs-and-cats-pet-grooming-brush",
+    imglink:
+      "https://img.btdmp.com/10312/10312716/products/0x540@16489890854caa125722.jpeg"
+  },
+  {
+    name: "14",
+    afflink:
+      "https://www.lazyshib.shop/collections/pets/products/usb-rechargeable-electric-nail-grinder-for-pet",
+    imglink:
+      "https://img.btdmp.com/10312/10312716/products/0x540@16489886198628d2a0d0.jpeg"
+  },
+  {
+    name: "13",
+    afflink:
+      "https://www.lazyshib.shop/collections/pets/products/electric-head-scalp-massage-multifunctional-vibrating-pet-massager",
+    imglink:
+      "https://img.btdmp.com/10312/10312716/products/0x540@16488838397cfc0c12d2.jpeg"
+  },
+  {
+    name: "12",
+    afflink:
+      "https://www.lazyshib.shop/collections/pets/products/cat-scratching-ball-toy-kitten-sisal-rope-ball-grinding-paws-toys",
+    imglink:
+      "https://img.btdmp.com/10312/10312716/products/0x540@16488838386d38d62810.png"
+  },
+  {
+    name: "11",
+    afflink:
+      "https://www.lazyshib.shop/collections/pets/products/smart-cat-toys-interactive-ball-catnip-playing-ball-training-toy",
+    imglink:
+      "https://img.btdmp.com/10312/10312716/products/0x540@1648883837e38a6ad4db.jpeg"
+  },
+  {
+    name: "10",
+    afflink:
+      "https://www.lazyshib.shop/collections/pets/products/pet-grooming-hammock-helper-restraint-bag-nail-clip",
+    imglink:
+      "https://img.btdmp.com/10312/10312716/products/0x540@1648884500ad37dad26c.png"
+  },
+  {
+    name: "9",
+    afflink:
+      "https://www.lazyshib.shop/collections/pets/products/jumping-activation-flashing-ball-light-sounds-dog-toysttps://www.lazyshib.shop/collections/pets/products/dog-sniffing-ball-puzzle-toys-increase-iq-slow-dispensing-feeder",
+    imglink:
+      "https://img.btdmp.com/10312/10312716/products/0x540@1648883837199731c898.gif"
+  },
+  {
+    name: "8",
+    afflink:
+      "https://www.lazyshib.shop/collections/pets/products/pet-grooming-tool-2-sided-undercoat-rake",
+    imglink:
+      "https://img.btdmp.com/10312/10312716/products/0x540@1648883836a1b667175e.png"
+  },
+  {
+    name: "7",
+    afflink:
+      "https://www.lazyshib.shop/collections/pets/products/dog-sniffing-ball-puzzle-toys-increase-iq-slow-dispensing-feeder",
+    imglink:
+      "https://img.btdmp.com/10312/10312716/products/0x540@16488838362933326186.png"
+  },
+  {
+    name: "6",
+    afflink:
+      "https://www.lazyshib.shop/collections/pets/products/professional-cat-nail-clipper-with-led-light",
+    imglink:
+      "https://img.btdmp.com/10312/10312716/products/0x540@1648883834ab6bd3b98d.png"
+  },
+  {
+    name: "5",
+    afflink:
+      "https://www.lazyshib.shop/collections/pets/products/double-shoulder-portable-travel-backpack-pet-dog-carrier",
+    imglink:
+      "https://img.btdmp.com/10312/10312716/products/0x540@16488838346cd5561229.png"
+  },
+  {
+    name: "4",
+    afflink:
+      "https://www.lazyshib.shop/collections/pets/products/pet-hair-grooming-comb-and-care-brush",
+    imglink:
+      "https://img.btdmp.com/10312/10312716/products/0x540@1648883833799ec58b75.png"
+  },
+  {
+    name: "3",
+    afflink:
+      "https://www.lazyshib.shop/collections/pets/products/portable-electric-cute-cat-shaped-ball-trimmer-clothes-hair-remover",
+    imglink:
+      "https://img.btdmp.com/10312/10312716/products/0x540@1648879564328b170598.png"
+  },
+  {
+    name: "2",
+    afflink:
+      "https://www.lazyshib.shop/products/perfume-atomizer-portable-liquid-container",
+    imglink:
+      "https://img.btdmp.com/10312/10312716/products/0x540@1648993054bc4846ad69.jpeg"
   }
 ];
 export const amazongallerytrending = [
