c7aff8650fdcb440b7c26c9f9dc335f38bb00694
huutai507 <huutai050798@gmail.com>
2022-05-31 17:37:16 +0700

    update

--- data/data.js
+++ data/data.js
@@ -2,7 +2,7 @@ export const data = [
   {
     name: '1',
     afflink:
-      'https://www.lazyshib.shop/collections/pets/products/usb-rechargeable-rgb-led-color-changing-light-up-pet-dog-collar-flashing-glow-glowing-luminous-night-dog-collars?variant=1000008332552424',
+      'https://www.amazon.com/Corsair-Vengeance-2x16GB-PC4-28800-Optimized/dp/B082DGZJ9C/?_encoding=UTF8&pd_rd_w=Q43RV&content-id=amzn1.sym.10f16e90-d621-4a53-9c61-544e5c741acc&pf_rd_p=10f16e90-d621-4a53-9c61-544e5c741acc&pf_rd_r=PTW126EX43K52ZC1CCBC&pd_rd_wg=SFpeS&pd_rd_r=ed3fab9f-a5a6-48ab-a0c3-91c3910d6df5&ref_=pd_gw_exports_top_sellers_unrec',
     imglink: '//ae01.alicdn.com/kf/H85072baf6fa34ef787e52111311a5d55m.jpg_350x350.jpg'
   }
 ];
