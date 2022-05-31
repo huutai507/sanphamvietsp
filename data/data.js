d2450f445ad011728085a702abaca653b796d604
huutai507 <huutai050798@gmail.com>
2022-05-31 18:35:00 +0700

    Update

--- data/data.js
+++ data/data.js
@@ -1,8 +1,22 @@
 export const data = [
   {
+    name: '4',
+    afflink: 'https://amzn.to/3GzMSeL',
+    imglink: 'https://m.media-amazon.com/images/I/41bW4wMywWL._SL350_.jpg'
+  },
+  {
+    name: '3',
+    afflink: 'https://amzn.to/3N5SwI5',
+    imglink: 'https://m.media-amazon.com/images/I/31PpfCeFm-L._SL350_.jpg'
+  },
+  {
+    name: '2',
+    afflink: 'https://amzn.to/3lW3KTJ',
+    imglink: 'https://m.media-amazon.com/images/I/51wK99-XHkL._SL350_.jpg'
+  },
+  {
     name: '1',
-    afflink:
-      'https://www.amazon.com/Corsair-Vengeance-2x16GB-PC4-28800-Optimized/dp/B082DGZJ9C/?_encoding=UTF8&pd_rd_w=Q43RV&content-id=amzn1.sym.10f16e90-d621-4a53-9c61-544e5c741acc&pf_rd_p=10f16e90-d621-4a53-9c61-544e5c741acc&pf_rd_r=PTW126EX43K52ZC1CCBC&pd_rd_wg=SFpeS&pd_rd_r=ed3fab9f-a5a6-48ab-a0c3-91c3910d6df5&ref_=pd_gw_exports_top_sellers_unrec',
-    imglink: '//ae01.alicdn.com/kf/H85072baf6fa34ef787e52111311a5d55m.jpg_350x350.jpg'
+    afflink: 'https://amzn.to/3LYqJId',
+    imglink: 'https://m.media-amazon.com/images/I/41PVemMeCML._SL350_.jpg'
   }
 ];
