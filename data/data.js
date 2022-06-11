7d9ea779562c3f54f01470908cd24c6af03db9d1
huutai507 <huutai050798@gmail.com>
2022-06-11 16:16:45 +0700

    update

--- data/data.js
+++ data/data.js
@@ -1,5 +1,11 @@
 export const data = [
   {
+    name: '2',
+    afflink: 'https://howdoibuythis.com/collections/most-wanted/products/couple-pillow?ref=Zpb0wsX2',
+    imglink:
+      'https://cdn.shopify.com/s/files/1/0559/6525/3702/products/H0d63c9b57d144169922376cf97c0edf9Q_1200x1200.jpg?v=1654885428'
+  },
+  {
     name: '1',
     afflink: 'https://howdoibuythis.com/products/car-headrest?ref=Zpb0wsX2',
     imglink: 'https://ae01.alicdn.com/kf/Sfa3a326a8f0646bf85ae17ecbfef47c3z.jpg_350x350.jpg'
