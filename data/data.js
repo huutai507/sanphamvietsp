8e5637972f1def2c16de3fb59aa8a79af1c86c10
huutai507 <huutai050798@gmail.com>
2022-06-11 16:27:57 +0700

    update

--- data/data.js
+++ data/data.js
@@ -1,5 +1,51 @@
 export const data = [
   {
+    name: '10',
+    afflink: 'https://howdoibuythis.com/collections/most-wanted/products/furniture-moving-tools?ref=Zpb0wsX2',
+    imglink: 'https://cdn.shopify.com/s/files/1/0559/6525/3702/products/454229555-1092149506_1200x1200.jpg?v=1653819632'
+  },
+  {
+    name: '9',
+    afflink: 'https://howdoibuythis.com/collections/most-wanted/products/drinking-bar?ref=Zpb0wsX2',
+    imglink:
+      'https://cdn.shopify.com/s/files/1/0559/6525/3702/products/Hf250ff14d5294786ba41dd7e085c69a3m_1200x1200.jpg?v=1654010130'
+  },
+  {
+    name: '8',
+    afflink: 'https://howdoibuythis.com/collections/most-wanted/products/airpods-kit?ref=Zpb0wsX2',
+    imglink:
+      'https://cdn.shopify.com/s/files/1/0559/6525/3702/products/S15d994739f334c8f8da804d829a0db9bO_1200x1200.jpg?v=1654431252'
+  },
+  {
+    name: '7',
+    afflink: 'https://howdoibuythis.com/collections/most-wanted/products/bubble-gun?ref=Zpb0wsX2',
+    imglink:
+      'https://cdn.shopify.com/s/files/1/0559/6525/3702/products/S671a05aa0f5642199e01724dc2c92337E_1200x1200.jpg?v=1653559948'
+  },
+  {
+    name: '6',
+    afflink:
+      'https://howdoibuythis.com/collections/most-wanted/products/usb-fly-catcher?ref=Zpb0wsX2&variant=39815164854342',
+    imglink:
+      'https://cdn.shopify.com/s/files/1/0559/6525/3702/products/usb-fly-catcher-963752_1200x1200.jpg?v=1644852812'
+  },
+  {
+    name: '5',
+    afflink: 'https://howdoibuythis.com/collections/most-wanted/products/sliding-bin?ref=Zpb0wsX2',
+    imglink: 'https://cdn.shopify.com/s/files/1/0559/6525/3702/products/sliding-bin-193096_1200x1200.jpg?v=1644850127'
+  },
+  {
+    name: '4',
+    afflink: 'https://howdoibuythis.com/collections/most-wanted/products/sliding-bin?ref=Zpb0wsX2',
+    imglink: 'https://cdn.shopify.com/s/files/1/0559/6525/3702/products/sliding-bin-193096_1200x1200.jpg?v=1644850127'
+  },
+  {
+    name: '3',
+    afflink: 'https://howdoibuythis.com/collections/most-wanted/products/modern-toilet-brush?ref=Zpb0wsX2',
+    imglink:
+      'https://cdn.shopify.com/s/files/1/0559/6525/3702/products/H0bdd1195ac44482c9b3354dfcfb694acB_1200x1200.jpg?v=1654789660'
+  },
+  {
     name: '2',
     afflink: 'https://howdoibuythis.com/collections/most-wanted/products/couple-pillow?ref=Zpb0wsX2',
     imglink:
