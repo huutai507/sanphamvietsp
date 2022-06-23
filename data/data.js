846a07a88feeecc943ecbaf6bea55e6e32a24228
huutai507 <huutai050798@gmail.com>
2022-06-23 10:52:15 +0700

    update

--- data/data.js
+++ data/data.js
@@ -1,5 +1,10 @@
 export const data = [
   {
+    name: '2',
+    afflink: 'https://amzn.to/3zVUXJy',
+    imglink: 'https://m.media-amazon.com/images/I/41dZ7Q5fU7L._SL350_.jpg'
+  },
+  {
     name: '1',
     afflink: 'https://amzn.to/3ybPeOw',
     imglink: 'https://m.media-amazon.com/images/I/31BeWJj3ipL._SL350_.jpg'
