c2053075eff47f4ac7988520022b6471a8766063
huutai507 <huutai050798@gmail.com>
2022-07-19 19:39:14 +0700

    update

--- data/data.js
+++ data/data.js
@@ -1,12 +1,7 @@
 export const data = [
   {
-    name: '2',
-    afflink: 'https://amzn.to/3zVUXJy',
-    imglink: 'https://m.media-amazon.com/images/I/41dZ7Q5fU7L._SL350_.jpg'
-  },
-  {
     name: '1',
-    afflink: 'https://amzn.to/3ybPeOw',
-    imglink: 'https://m.media-amazon.com/images/I/31BeWJj3ipL._SL350_.jpg'
+    afflink: 'https://amzn.to/3Pi46kH',
+    imglink: 'https://m.media-amazon.com/images/I/41AaCMFt7WL._SL350_.jpg'
   }
 ];
