74c57085231d83607cbd5f555e37f0e0ab4719c5
huutai507 <huutai050798@gmail.com>
2022-07-29 14:33:26 +0700

    update

--- pages/index.js
+++ pages/index.js
@@ -94,18 +94,14 @@ export default function Home() {
                   </p>
                   <h4 className='north-america'>GLOBAL</h4>
                   <a href={item.usalink} target='_blank' className='custom-button'>
-                    View Prices on Amazon
+                    View Cheap Prices on Amazon
+                  </a>
+                  <a href={item.aliexpress} target='_blank' className='custom-button aliexpress'>
+                    View Prices on Aliexpress
                   </a>
                   <h4 className='north-america'>AMERICA</h4>
                   <a href={item.usalink} target='_blank' className='custom-button'>
                     USA ‣ View Prices on Amazon
-                  </a>
-                  <a
-                    href='https://www.alenascarlets.com/collections/phonecase'
-                    target='_blank'
-                    className='custom-button plushies'
-                  >
-                    TOP 10 Best Seller iPhone Case
                   </a>
                   <a href={replaceArea(item.usalink, 'amazon.ca')} target='_blank' className='custom-button'>
                     Canada ‣ View Prices on Amazon
@@ -159,16 +155,12 @@ export default function Home() {
                     <a href={item.usalink} target='_blank' className='custom-button'>
                       View Prices on Amazon
                     </a>
+                    <a href={item.aliexpres} target='_blank' className='custom-button aliexpress'>
+                      View Cheap Prices on Aliexpress
+                    </a>
                     <h4 className='north-america'>AMERICA</h4>
                     <a href={item.usalink} target='_blank' className='custom-button'>
                       USA ‣ View Prices on Amazon
-                    </a>
-                    <a
-                      href='https://www.alenascarlets.com/collections/phonecase'
-                      target='_blank'
-                      className='custom-button plushies'
-                    >
-                      TOP 10 Best Seller iPhone Case
                     </a>
                     <a href={replaceArea(item.usalink, 'amazon.ca')} target='_blank' className='custom-button'>
                       Canada ‣ View Prices on Amazon
