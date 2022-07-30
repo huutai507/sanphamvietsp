894d5ffca1e45e4825158367529879556732c8b0
huutai507 <huutai050798@gmail.com>
2022-07-30 07:02:54 +0700

    update

--- pages/index.js
+++ pages/index.js
@@ -103,7 +103,7 @@ export default function Home() {
                   </a>
                   {item.aliexpress && (
                     <a href={item.aliexpress} target='_blank' className='custom-button aliexpress'>
-                      View Good Prices on Aliexpress
+                      View Cheap Prices on Aliexpress
                     </a>
                   )}
 
@@ -169,7 +169,7 @@ export default function Home() {
                     </a>
                     {item.aliexpress && (
                       <a href={item.aliexpress} target='_blank' className='custom-button aliexpress'>
-                        View Good Prices on Aliexpress
+                        View Cheap Prices on Aliexpress
                       </a>
                     )}
                     <h4 className='north-america'>AMERICA</h4>
