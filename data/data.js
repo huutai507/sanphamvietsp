83113420480b735fff709f69c82ebb3800ab05b5
huutai507 <huutai050798@gmail.com>
2023-04-16 14:50:59 +0700

    update

--- data/data.js
+++ data/data.js
@@ -2,22 +2,17 @@ export const data = [
   {
     name: '1',
     usalink: '',
-    imglink: 'https://m.media-amazon.com/images/I/41AaCMFt7WL._SL350_.jpg',
+    imglink:
+      'https://ae01.alicdn.com/kf/S28f2909f346b41c3bd7106e457f357ba0/2023-New-White-Iron-Man-Cosplay-1-1-Mk5-War-Machine-Helmet-Electric-Helmet-Open-Close.jpg_350x350.jpg_.webp',
     shortlink: '',
-    customlink: 'as'
+    customlink: 'https://shope.ee/2VLdHZDgpS'
   },
   {
-    name: '12',
+    name: '1',
     usalink: '',
-    imglink: 'https://m.media-amazon.com/images/I/41AaCMFt7WL._SL350_.jpg',
+    imglink:
+      'https://ae01.alicdn.com/kf/S7ea0ca43e2a846b6926c9761bdb5bf6dQ/2023-New-Bumblebee-Chinese-english-Dual-Control-Transformers-Action-Figure-Cosplay-Helmet-Limited-Edition-Collect-Ornaments.jpg_350x350.jpg_.webp',
     shortlink: '',
-    customlink: 'as'
-  },
-  {
-    name: '122',
-    usalink: '',
-    imglink: 'https://m.media-amazon.com/images/I/41AaCMFt7WL._SL350_.jpg',
-    shortlink: '',
-    customlink: 'as'
+    customlink: 'https://shope.ee/qDPIK2Tn0'
   }
 ];
